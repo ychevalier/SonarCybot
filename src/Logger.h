f96485184f04db9ebba7bb7f210b22a1c13538ec
Yann <yann.m.chevalier@gmail.com>
2020-11-22 20:48:05 +0100

    Refactoring DummyLogger in Logger.h

--- src/Logger.h
+++ src/Logger.h
@@ -7,4 +7,10 @@ public:
   virtual void log(const char* message) = 0;
 };
 
+class DummyLogger : public Logger {
+public:
+  void log(int message) override {}
+  void log(const char* message) override {}
+};
+
 #endif // LOGGER_H
--- src/Thinker.h
+++ src/Thinker.h
@@ -6,12 +6,6 @@
 
 #include "Logger.h"
 
-class DummyLogger : public Logger {
-public:
-  void log(int message) override {}
-  void log(const char* message) override {}
-};
-
 class Thinker {
 public:
   Thinker(Sensor* sensor, Actuator* actuator, Logger* logger = new DummyLogger());
